71628b4f9252e80b269135e2415629ae78999fc5
SPL_DM\jason.mccandless <jason.mccandless@spl-inc.com>
2022-12-05 16:56:35 -0600

    fixed the dates input fields. will not not do the autocomplete right now

--- src/components/LookupForm.jsx
+++ src/components/LookupForm.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { LocationNamingMap } from "../util/constants";
 import { Autocomplete, Box, FormControl, TextField } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 
 function LookupForm({ searchResults, handleChange, searchObj, compareScreen }) {
   // this function allows the field to read the value as it updates from CompareTable
   function handleName(obj, k) {
     return obj[`${k}`];
+  }
+
+  const [selectedDate, setSelectedDate] = useState();
+
+  function handleDateSelect(newDate) {
+    setSelectedDate(newDate);
   }
 
   return (
@@ -26,20 +34,36 @@ function LookupForm({ searchResults, handleChange, searchObj, compareScreen }) {
             justifyContent: "space-around",
           }}
         >
-          {Object.entries(LocationNamingMap).map(([key, val], i) => (
-            <TextField
-              sx={{ margin: "5px 10px" }}
-              key={i}
-              size="small"
-              // margin="dense"
-              variant="outlined"
-              onChange={handleChange}
-              label={val}
-              id={key}
-              name={key}
-              value={handleName(searchObj, key)}
-            />
-          ))}
+          {Object.entries(LocationNamingMap).map(([key, val], i) => {
+            if (key === "effectiveStartDate" || key === "effectiveEndDate") {
+              return (
+                <LocalizationProvider key={i} dateAdapter={AdapterDayjs}>
+                  <DesktopDatePicker
+                    // size="small"
+                    label="Date desktop"
+                    inputFormat="MM/DD/YYYY"
+                    value={selectedDate}
+                    onChange={handleDateSelect}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              );
+            }
+            return (
+              <TextField
+                sx={{ margin: "5px 10px" }}
+                key={i}
+                // size="small"
+                // margin="dense"
+                variant="outlined"
+                onChange={handleChange}
+                label={val}
+                id={key}
+                name={key}
+                value={handleName(searchObj, key)}
+              />
+            );
+          })}
         </FormControl>
       </Box>
     </>
