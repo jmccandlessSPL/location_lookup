c223bceb98e81656571995b1ace0103cedbf4f04
SPL_DM\jason.mccandless <jason.mccandless@spl-inc.com>
2022-12-12 16:58:45 -0600

    added the ability to edit the selected location. still need field specific options for date and types and such

--- src/components/SelectLcoationTable.jsx
+++ src/components/SelectLcoationTable.jsx
@@ -1,29 +1,25 @@
 import React, { useEffect, useState } from "react";
-import {
-  Box,
-  Button,
-  Card,
-  CardHeader,
-  styled,
-  Table,
-  TableBody,
-  TableCell,
-  tableCellClasses,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
+import { Box, Button, styled, TextField } from "@mui/material";
 
 import "react-diff-view/style/index.css";
 import { LocationNamingMap } from "../util/constants";
 
+const EditTableTextArea = styled(TextField)(({ theme }) => ({
+  "& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input": {
+    padding: "0 5px",
+  },
+  // padding: "0",
+}));
+
 function SelectLocationTable({ objCompare }) {
+  const [canEdit, setCanEdit] = useState(false);
+
+  const [editableObj, setEditableObj] = useState({});
+
   function nestedObjManipNONORIG(obj) {
     const flattened = {};
-
     Object.keys(obj).forEach((key) => {
       const value = obj[key];
-
       if (
         typeof value === "object" &&
         value !== null &&
@@ -34,15 +30,40 @@ function SelectLocationTable({ objCompare }) {
         flattened[key] = value;
       }
     });
-
     return flattened;
   }
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setEditableObj(nestedObjManipNONORIG(objCompare));
+  }, [objCompare]);
+
+  function handleEditButton(e) {
+    e.preventDefault();
+    setCanEdit(true);
+  }
+
+  function handleSaveButton(e) {
+    setCanEdit(false);
+  }
+
+  function handleChange(e) {
+    e.preventDefault();
+    setEditableObj({ ...editableObj, [`${e.target.name}`]: e.target.value });
+  }
 
   return (
     <>
-      <h3>Single Location</h3>
+      <Box
+        className="tab-header"
+        display="grid"
+        gridTemplateColumns="1fr 3fr 1fr"
+      >
+        <h3 style={{ gridColumn: "2" }}>Single Location</h3>
+        <Box className="edit-data-box" alignSelf="center">
+          <button onClick={handleSaveButton}>save</button>
+          <button onClick={handleEditButton}>edit</button>
+        </Box>
+      </Box>
       <Box className="compare-table-box" height="400px">
         <table style={{ height: "inherit", width: "100%" }}>
           <tbody className="select-local-table">
@@ -74,8 +95,25 @@ function SelectLocationTable({ objCompare }) {
                       </Box>
                     </td>
                     <td className="att-name" align="left">
-                      <div className="pre-style">
-                        {nestedObjManipNONORIG(objCompare)[`${rowTitle}`]}
+                      <div
+                        className="pre-style"
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <EditTableTextArea
+                          sx={{
+                            "& .css-1d3z3hw-MuiOutlinedInput-notchedOutline":
+                              canEdit
+                                ? { borderWidth: "1px", borderStyle: "solid" }
+                                : { border: "none" },
+                          }}
+                          name={rowTitle}
+                          disabled={!canEdit}
+                          value={editableObj[`${rowTitle}`] || ""}
+                          onChange={(e) => handleChange(e)}
+                        />
                       </div>
                     </td>
                   </tr>
